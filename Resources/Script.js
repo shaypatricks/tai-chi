5215a53026ac4fdff0d59ac844b95bd85ae77387
Steffen Hayes <31227039+shaypatricks@users.noreply.github.com>
2018-11-30 11:30:15 -0600

    Add files via upload

---
+++ Resources/Script.js
@@ -0,0 +1,7 @@
+$('#corrections').on('shown.bs.modal', function () {
+    $('#fourcorrections').trigger('focus')
+  })
+
+  $('#Nei').on('shown.bs.modal', function () {
+    $('#screwup').trigger('focus')
+  })
